7459b906820f54b1dcaffb9a795fad7649bc84bd
Leandro Pedroso <lpedroso64@outlook.com>
2023-01-10 14:57:58 -0300

    commenting functions for reading

--- C.R.U.D.py
+++ C.R.U.D.py
@@ -23,8 +23,14 @@ cursor.close()
 conexao.close()
 
 # CREATE
-nome_produto = "leite"
-valor = 4
-comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+#nome_produto = "leite"
+#valor = 4
+#comando = f'INSERT INTO vendas (nome_produto, valor) VALUES ("{nome_produto}", {valor})'
+#cursor.execute(comando)
+#conexao.commit() # Para editar banco de dados.
+
+# READ
+comando = f'SELECT * FROM vendas'
 cursor.execute(comando)
-conexao.commit() # Para editar banco de dados.
+resultado = cursor.fetchall() # Ler o banco de dados.
+print(resultado)
