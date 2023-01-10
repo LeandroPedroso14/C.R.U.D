0c3e962464c74ecfb64ef7e57f5a6926810712a9
Leandro Pedroso <lpedroso64@outlook.com>
2023-01-10 15:33:14 -0300

    updating the database

--- C.R.U.D.py
+++ C.R.U.D.py
@@ -10,12 +10,12 @@ database='bancoestudo',
 cursor = conexao.cursor()
 
 # CRUD
-
-comando = f'SELECT * FROM vendas'
+nome_produto = "café"
+valor = 7
+comando = f'UPDATE vendas SET valor = {valor} WHERE nome_produto = "{nome_produto}"'
 cursor.execute(comando)
-#conexao.commit() # Para editar banco de dados.
-resultado = cursor.fetchall() # Ler o banco de dados.
-print(resultado)
+conexao.commit() # Para editar banco de dados.
+
 
 
 cursor.close()
@@ -29,3 +29,9 @@ conexao.close()
 #conexao.commit() # Para editar banco de dados.
 
 # READ
+#comando = f'SELECT * FROM vendas'
+#cursor.execute(comando)
+#resultado = cursor.fetchall() # Ler o banco de dados.
+#print(resultado)
+
+# UPDATE
